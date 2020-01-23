5320b7c451260099e987e57c8245747417e2ff87
tristan1940252 <trit0tv0@gmail.com>
2020-01-23 15:16:45 -0500

    Commit de base

---
+++ Tableaux.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Tableaux {
+    public static int faire(int i){
+        return i++;
+    }
+
+    public static String faire (String s){
+        return s.concat("+1");
+    }
+}
